b63fde4dd18ed506a47f53ff45f2e075194a4da7
duranhumes <duranhumes@gmail.com>
2018-12-05 10:00:56 -0500

    Simplified genSchemas and added function to clear tables for testing

--- api/src/database.ts
+++ api/src/database.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import { createConnection, ConnectionOptions, Connection } from 'typeorm'
+
 import { logger } from './lib/utils/logging'
 
 const isProduction = process.env.NODE_ENV === 'production'
@@ -9,6 +10,7 @@ const database = isTesting
     ? String(process.env.MYSQL_DATABASES_TEST)
     : String(process.env.MYSQL_DATABASES)
 const projectDirBasePath = path.normalize(path.resolve(__dirname, '..'))
+console.log(database)
 
 class Database {
     connectionOptions: ConnectionOptions
@@ -25,7 +27,7 @@ class Database {
             port: Number(process.env.MYSQL_PORT),
             username: String(process.env.MYSQL_ROOT_USER),
             password: String(process.env.MYSQL_PASSWORD),
-            logging: !isProduction,
+            logging: isProduction && isTesting,
             synchronize: true,
         }
         this.connection = new Connection(this.connectionOptions)
@@ -33,9 +35,13 @@ class Database {
 
     public async init() {
         await this.openConnection()
-        this.genModelSchemas()
+        await this.genModelSchemas()
 
         console.log('DB successfully connected')
+    }
+
+    public get isConnected() {
+        return this.connection.isConnected
     }
 
     public async openConnection() {
@@ -63,60 +69,41 @@ class Database {
         }
     }
 
+    public async clearTables() {
+        const entities = this.connection.entityMetadatas
+
+        for (const entity of entities) {
+            const repository = await this.connection.getRepository(entity.name)
+            await repository.query(`TRUNCATE TABLE ${entity.tableName};`)
+        }
+    }
+
     /**
      * Loop through all models and gen schemas
      * to validate against later
      */
-    private genModelSchemas() {
-        const modelsDir = `${projectDirBasePath}/src/models`
+    private async genModelSchemas() {
         const schemasDir = `${projectDirBasePath}/src/schemas`
 
-        let files: string[] = []
-        try {
-            files = fs.readdirSync(modelsDir)
-        } catch (err) {
-            console.log(err)
+        const entities = this.connection.entityMetadatas
+        for (const entity of entities) {
+            const modelSchema = this.connection.getMetadata(entity.name)
+                .propertiesMap
 
-            process.exit(0)
-        }
+            const objKeys = Object.keys(modelSchema)
+            const keys = objKeys.map((k: any) => `'${k}'`)
+            const template = `export default [${keys}]`
 
-        /**
-         * Filter out folders and files with
-         * only first letter being capital
-         * and with name not being 'Model'
-         */
-        const models = files.filter((file: string) => {
-            const firstLetter = file.charAt(0)
+            const schemaFile = `${schemasDir}/${entity.name}Schema.ts`
+            try {
+                fs.writeFileSync(schemaFile, template)
 
-            return firstLetter === firstLetter.toUpperCase() && file !== 'Model'
-        })
+                console.log(`${entity.name} schema created!`)
+            } catch (err) {
+                console.log(err)
 
-        /**
-         * Loop through files found and use
-         * typeorm connection to get schema data
-         * and write to file in schemas folder
-         */
-        if (models.length > 0) {
-            models.forEach((model: string) => {
-                const modelSchema = this.connection.getMetadata(model)
-                    .propertiesMap
-
-                const keys = (Object.keys(modelSchema) as any).map(
-                    (k: any) => `'${k}'`
-                )
-                const template = `export default [${keys}]`
-
-                const schemaFile = `${schemasDir}/${model}Schema.ts`
-                try {
-                    fs.writeFileSync(schemaFile, template)
-
-                    console.log(`${model} schema created!`)
-                } catch (err) {
-                    console.log(err)
-
-                    process.exit(0)
-                }
-            })
+                process.exit(0)
+            }
         }
     }
 }
