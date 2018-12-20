20adcfacbc43f4512f055d94ac1842ac481e6749
duranhumes <duranhumes@gmail.com>
2018-12-20 14:45:01 -0500

    updated endpoints

--- web/src/api/Endpoints.ts
+++ web/src/api/Endpoints.ts
@@ -1,4 +1,9 @@
 const BASE_URL = 'http://localhost:8080/v1'
 
-export const userEndpoint = `${BASE_URL}/users`
+export const baseEndpoint = BASE_URL
+export const healthEndpoint = `${BASE_URL}/health`
+export const usersEndpoint = `${BASE_URL}/users`
 export const loginEndpoint = `${BASE_URL}/login`
+export const tracksEndpoint = `${BASE_URL}/tracks`
+export const streamEndpoint = `${BASE_URL}/stream`
+export const albumsEndpoint = `${BASE_URL}/albums`
